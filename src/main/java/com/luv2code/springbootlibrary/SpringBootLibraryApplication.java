bb64a1453d9baae6632b310b1a245e329b33a7c6
pakli <pakli991088@gmail.com>
2023-09-15 17:58:08 +0800

    first commit

---
+++ src/main/java/com/luv2code/springbootlibrary/SpringBootLibraryApplication.java
@@ -0,0 +1,15 @@
+package com.luv2code.springbootlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
+import java.lang.*;
+
+@SpringBootApplication
+public class SpringBootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryApplication.class, args);
+	}
+		String string = "123";
+}
